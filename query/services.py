03f7e7166595823bb6aa14e1b7b0128f0eb7f844
Doridian <git@doridian.net>
2025-11-07 16:31:21 -0800

    Ignore units wanted by wrong targets

--- query/services.py
+++ query/services.py
@@ -3,6 +3,7 @@ from os.path import basename
 from dataclasses import dataclass
 
 IGNORE_STOPPED_SERVICE_TYPES = {b'dbus'}
+IGNORE_TARGETS = {b'sleep.target', b'shutdown.target', b'timers.target'}
 
 @dataclass(kw_only=True, frozen=True)
 class Service:
@@ -17,7 +18,12 @@ class Service:
         return Service(name=unit_name.removesuffix(b'.service'), unit=unit)
     
     def is_down(self) -> bool:
-        return self.unit.Unit.ActiveState != b'active' and self.unit.Unit.LoadState == b'loaded' and self.unit.Service.Type not in IGNORE_STOPPED_SERVICE_TYPES
+        wantedBy = set([w.strip() for w in self.unit.Unit.WantedBy])
+        wantedBySignificant = wantedBy - IGNORE_TARGETS
+        return self.unit.Unit.ActiveState != b'active' and \
+                self.unit.Unit.LoadState == b'loaded' and \
+                self.unit.Service.Type not in IGNORE_STOPPED_SERVICE_TYPES and \
+                wantedBySignificant
 
     def __repr__(self):
         return f"Service(name={self.name}, type={self.unit.Service.Type}, active={self.unit.Unit.ActiveState}, load={self.unit.Unit.LoadState})"
